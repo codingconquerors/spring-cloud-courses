1717d2111e5128862ae572a7a64dafac052079f6
codingconquerors <100064563+codingconquerors@users.noreply.github.com>
2022-07-23 00:34:12 -0400

    currency-exchange-service-intial-version

---
+++ currency-exchange-service/currency-exchange-service/src/test/java/com/cloud/spring/microservices/currencyexchange/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.spring.microservices.currencyexchange.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
