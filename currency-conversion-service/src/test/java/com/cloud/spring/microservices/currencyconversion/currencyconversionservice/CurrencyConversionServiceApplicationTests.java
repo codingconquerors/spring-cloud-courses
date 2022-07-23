aa33b8f01c8cac34a09698452a4268c65bab82b9
codingconquerors <100064563+codingconquerors@users.noreply.github.com>
2022-07-23 01:48:44 -0400

    conversion service initial version

---
+++ currency-conversion-service/src/test/java/com/cloud/spring/microservices/currencyconversion/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.spring.microservices.currencyconversion.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
