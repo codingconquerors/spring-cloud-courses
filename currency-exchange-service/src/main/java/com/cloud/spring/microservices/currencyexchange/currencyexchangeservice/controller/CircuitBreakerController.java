cda814c6168fe131697680f8a7bca22ca86868a7
khushwinder <chahalkhushwinder@gmail.com>
2022-08-03 22:20:32 -0400

    circuit breaker

--- currency-exchange-service/src/main/java/com/cloud/spring/microservices/currencyexchange/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/cloud/spring/microservices/currencyexchange/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -20,19 +20,24 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // circuit breaker with default configuration
     //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-    @RateLimiter(name="default")
+
+    //Rate limit with default configuration
+   // @RateLimiter(name="default")
     // bulk head for concurrent calls allowed
-    //@Bulkhead(name="sample-api")
+    @Bulkhead(name="sample-api", fallbackMethod = "hardcodedResponse")
     //10s => 10000 calls to the sample api
     public String sampleApi() {
         logger.info("Sample api call received");
 
         // want to intimate the behavior of a service which is not working
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+       /* ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         logger.info("Sample api call processed");
-		return forEntity.getBody();
-      //  return "sample-api";
+		return forEntity.getBody();*/
+
+		// enable below for rate limit example
+        return "sample-api";
     }
 
     // return type should be same as main method otherwise runtime exception will be thrown
